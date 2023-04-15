f558cb4e4b342e25646c21a199e501d7cb107670
omar3o3 <otantawy53@gmail.com>
2023-04-15 17:59:21 -0400

    updated form to handle errors

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-add-task',
@@ -12,25 +13,25 @@ export class AddTaskComponent implements OnInit{
   showWarning: boolean = false;
   // console.log(text)
 
-  onSubmit() {
-    if (!this.text && !this.day) {
-      // console.log('you need to provide a title and day')
+  onSubmit(myForm: NgForm) {
+    if (!this.text || !this.day) {
       console.log('NOT from filled')
       this.showWarning = true
     }
     if(this.text && this.day) {
-      // console.log(this.text, this.day, this.reminder)
       console.log('from filled')
       this.showWarning = false
+      // const newTask = {
+      //   text: this.text,
+      //   day: this.day,
+      //   reminder: this.reminder
+      // }
       const newTask = {
-        text: this.text,
-        day: this.day,
-        reminder: this.reminder
+        text: myForm.value.text,
+        day: myForm.value.day,
+        reminder: myForm.value.reminder
       }
-
-      this.text = ''
-      this.day = ''
-      this.reminder = false
+      myForm.resetForm();
     }
   }
 
